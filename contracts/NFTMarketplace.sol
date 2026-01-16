3eb8c3453f2c87d75b5a0002b7c3531be30fd648
KP <chn24mc015@ceconline.edu>
2026-01-16 13:54:21 +0530

    sprint3 fixed payable seller and buy nft logic

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -7,11 +7,42 @@ contract NFTMarketplace is ERC721 {
 
     uint256 private tokenIdCounter;
 
+    struct MarketItem {
+        uint256 tokenId;
+        address payable seller;
+        uint256 price;
+        bool sold;
+    }
+
+    mapping(uint256 => MarketItem) public marketItems;
+
     constructor() ERC721("MyNFT", "MNFT") {}
 
     function mintNFT(address owner) public returns (uint256) {
         tokenIdCounter += 1;
         _mint(owner, tokenIdCounter);
         return tokenIdCounter;
+    }
+
+    function listNFT(uint256 tokenId, uint256 price) public {
+        require(ownerOf(tokenId) == msg.sender, "Not owner");
+
+        marketItems[tokenId] = MarketItem(
+            tokenId,
+            payable(msg.sender),
+            price,
+            false
+        );
+    }
+
+    function buyNFT(uint256 tokenId) public payable {
+        MarketItem storage item = marketItems[tokenId];
+
+        require(!item.sold, "Already sold");
+        require(msg.value == item.price, "Incorrect price");
+
+        item.seller.transfer(msg.value);
+        _transfer(item.seller, msg.sender, tokenId);
+        item.sold = true;
     }
 }
